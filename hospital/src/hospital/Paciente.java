a1b1ec8891df9410acffc14e6dcf172674a480f0
31586899 <31586899@093075-P31S405.macklabs.br>
2017-09-27 19:53:21 -0300

    classes criadas

---
+++ hospital/src/hospital/Paciente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital;
+
+import java.util.Date;
+
+/**
+ *
+ * @author 31586899
+ */
+public class Paciente {
+    private Date dataEntrada;
+    private Date dataSaida;
+    private Date horaEntrada;
+    private Date horaSaida;
+    private String planoSaude;
+    private Double valorDiaria;
+    private String leito;
+    
+}
